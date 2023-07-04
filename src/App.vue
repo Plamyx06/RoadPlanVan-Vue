0feabc515e909c1c6246f0f9cbf15ad66705967a
plamyx <albanese.aurelien06@gmail.com>
2023-07-04 11:19:46 +0200

    app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/Homepage.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/product">Product</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/components/Homepage.vue
@@ -0,0 +1,10 @@
+<template>
+      <div class="relative">
+        <img class="object-cover min-w-[320px] min-h-[500px] max-h-[800px] w-full " src="@/assets/main-img.jpg" alt="Votre image"/>
+        <h1 class="absolute inset-0 flex justify-center items-center text-center text-white font-bold text-4xl md:text-6xl" style="-webkit-text-stroke: 0.5px #ffffff; font-family: 'Kalam', cursive; color: #8A4852;">
+          ROADPLANVAN<br>
+          TON COMPAGNON DE VOYAGE<br>
+          planifie, explore, profite !
+        </h1>
+      </div>
+</template>
